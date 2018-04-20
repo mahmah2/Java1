3cae4a3678e384fe434a088b2fe145888ebe3e8d
Amir <mahfoozi@gmail.com>
2018-04-20 16:17:21 +1000

    test unit for product

---
+++ test/ProductTests.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Van.Product.Servlet.Product;
+import java.math.BigDecimal;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class ProductTests {
+    
+    public ProductTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void TestProductCreation() {
+        Product product = new Product(1, "APF", "Automatic Paper Feed", new BigDecimal(50.66));
+        Assert.assertEquals(2, product.getId());
+    }
+}
